f638b1b00b6dec1b97855b6878b7a4c7ef9eb10e
luke.isla <luke.isla@gmail.com>
2013-08-01 09:33:20 -0700

    Implemented, troubleshooted and completed 7.6.3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -63,6 +63,14 @@ describe "UserPages" do
       it "should not have content 'error'" do
         expect { should_not have_content('error') }
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) {User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
   	end
 
     describe "verbose password confirmation blank error test" do
