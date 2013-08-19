527faed73ceac484b13efa74c27c2171821a11ab
luke.isla <luke.isla@gmail.com>
2013-08-19 12:42:41 -0700

    Halfway through 8.5.2, need to add signup method.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,9 +28,7 @@ describe "Authentication" do
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
-				fill_in "Email",	with: user.email.upcase
-				fill_in "Password",	with: user.password
-				click_button "Sign In"
+				valid_signin(user)
 			end
 
 			it { should have_selector('title', text: user.name)}
