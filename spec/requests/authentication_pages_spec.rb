dcf6e5f79b22b3392562c85a9a9d8f5b7e532c16
islador <luke.isla@gmail.com>
2013-09-03 12:22:34 -0700

    Completed 9.22 and 9.23

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,5 +63,41 @@ describe "Authentication" do
 				end
 			end
 		end
+
+		describe "as wrong user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+			before { sign_in user, no_capybara: true}
+
+			describe "visiting Users#edit page" do
+				before { visit edit_user_path(wrong_user) }
+				it { should_not have_selector('title',	text: 'Edit user')}
+			end
+
+			describe "submitting a PUT request to the Users#update action" do
+				before { put user_path(wrong_user) }
+				specify {expect(response).to redirect_to(root_url) }
+			end
+		end
+
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "when attempting to visit a protected page" do
+				before do
+					visit edit_user_path(user)
+					fill_in "Email",	with: user.email
+					fill_in "Password",	with: user.password
+					click_button "Sign In"
+				end
+
+				describe "after signing in" do
+
+					it "should render the desired protected page" do
+						expect {page.to have_selector('title',	text: 'Edit user')}
+					end
+				end
+			end
+		end
 	end
 end
