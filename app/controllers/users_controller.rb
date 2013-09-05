e5eae85408d3504cc21e8d969eef2f88ddb54237
islador <luke.isla@gmail.com>
2013-09-04 17:09:50 -0700

    Completed 9.4.2, tests passing, site renders an error for an admin user on /users/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user,  only: [:index, :edit, :update]
+  before_filter :signed_in_user,  only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,  only: [:edit, :update]
-  
+  before_filter :admin_user,  only: :destroy
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -60,4 +67,7 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
 
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
