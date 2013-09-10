e9f244bdaaa59d9f5cc0334a6175634030c7c028
islador <luke.isla@gmail.com>
2013-09-09 19:21:07 -0700

    10.1.2, all tests green.

---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Micropost do
+	let(:user) {FactoryGirl.create(:user) }
+	before do
+		#This code is not idiomatically correct.
+		@micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+		#Answer: it's not right because it's creating a single piece of content instead
+		# of dynamic content?
+	end
+
+	subject { @micropost }
+
+	it { should respond_to(:content) }
+	it { should respond_to(:user_id) }
+
+	it { should be_valid }
+
+	describe "when user id is not present" do
+		before { @micropost.user_id = nil }
+		it { should_not be_valid }
+	end
+
+end
