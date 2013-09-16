46026b0a387b7bb78096b9e73c35bb8c16de545d
islador <luke.isla@gmail.com>
2013-09-16 16:07:56 -0700

    Finished 11.1.3, all tests green.

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -24,4 +24,16 @@ describe Relationship do
   	its(:follower) {should == follower }
   	its(:followed) {should == followed }
   end
+
+  #added in 11.1.3
+  describe "when followed id is not present" do
+  	before { relationship.followed_id = nil }
+  	it {should_not be_valid}
+  end
+
+  describe "when follower id is not present" do
+  	before { relationship.follower_id = nil }
+  	it {should_not be_valid}
+  end
+  
 end
