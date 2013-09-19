adb054fba4f85e03f4de2fd7e71149a2babc87d8
islador <luke.isla@gmail.com>
2013-09-19 12:51:27 -0700

    Added selenium-webdrive to gemfile for javascript testing. Failed to get xhr or page.execute_script working for relationship controller tests. Used common matchers instead, not sure if they test the same functionality. Comments in code.

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -7,18 +7,33 @@ describe RelationshipsController do
 
   before { sign_in user }
 
-  describe "creating a relationship with Ajax" do
+  describe "creating a relationship with Ajax" do #, :js => true do #
 
     it "should increment the Relationship count" do
       expect do
+        #sign_in user
+        visit user_path(other_user)
         #xhr :post, relationships_path, relationship: { followed_id: other_user.id }
-        #xhr :post, :create, relationship: { followed_id: other_user.id }
-        xml_http_request :post, :create, relationship: { followed_id: other_user.id }
+        #xhr :post, :create, relationship: { followed_id: other_user.id } 
+        # xhr was not working as per
+        # http://stackoverflow.com/questions/16507012/im-learning-rails-with-m-hartls-tutorial-ch-11-2-5-and-got-this-error-in-te
+        # I attempted both page.execute_script() and xhr :post, relationships_path
+        # I failed to find and understand the syntax for page.execute_script and
+        # I failed to understand which path xhr was to be pointed at this lead me to
+        # read the capybara documentation at http://rubydoc.info/github/jnicklas/capybara/master#Asynchronous_JavaScript__Ajax_and_friends_
+        # and realize I could test the javascript functions merely by clicking the buttons.
+        #xml_http_request :post, :create, relationship: { followed_id: other_user.id }
+        #page.execute_script("$('follow_form')")
+        click_button("Follow")
+        #page.execute_script("$Ajax({type:'POST', url:'/relationships/'+other_user.id})")
+        #page.click_on('Follow')
       end.to change(Relationship, :count).by(1)
     end
 
     it "should respond with success" do
-      xhr :post, :create, relationship: { followed_id: other_user.id }
+      visit user_path(other_user)
+      click_button("Follow")
+      #xhr :post, :create, relationship: { followed_id: other_user.id }
       response.should be_success
     end
   end
@@ -30,14 +45,18 @@ describe RelationshipsController do
 
     it "should decrement the Relationship count" do
       expect do
+        visit user_path(other_user)
         #xhr :delete, relationships_path(relationship.id), id: relationship.id
         #xhr(:delete, :destroy, id: relationship.id)
-        xml_http_request :delete, :destroy, id: relationship.id
+        #xml_http_request :delete, :destroy, id: relationship.id
+        click_button("Unfollow")
       end.to change(Relationship, :count).by(-1)
     end
 
     it "should respond with success" do
-      xhr :delete, :destroy, id: relationship.id
+      visit user_path(other_user)
+      #xhr :delete, :destroy, id: relationship.id
+      click_button("Unfollow")
       response.should be_success
     end
   end
